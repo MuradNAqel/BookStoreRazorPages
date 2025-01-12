8d99c39635dc1800586dc3335817a98b9075e33d
MuradNAqel <muradaqel.98@gmail.com>
2025-01-12 14:48:52 +0300

    Add working Not dynamically

--- BookStoreRazorPages/Application/Dtos/AuthorDtos/AuthorDto.cs
+++ BookStoreRazorPages/Application/Dtos/AuthorDtos/AuthorDto.cs
@@ -11,5 +11,32 @@ namespace BookStoreRazorPages.Application.Dtos.AuthorDtos
         public string ImagePath { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Nationality Nationality { get; set; }
+        //public int BookId { get; set; }
+
+        public Author MapToAuthor()
+        {
+            return new Author(
+                    name: Name,
+                    dateOfBirth: DateOfBirth,
+                    biography: Biography,
+                    nationality: Nationality,
+                    speciality: Speciality
+                );
+        }
+
+        public AuthorDto MapToAuthorDto(Author author)
+        {
+            return new AuthorDto
+            {
+                ImagePath = author.ImagePath,
+                Biography = author.Biography,
+                DateOfBirth = author.DateOfBirth,
+                Nationality = author.Nationality,
+                Speciality = author.Speciality,
+                Name = author.Name,
+                Id = author.Id,
+            };
+        }
+
     }
 }
